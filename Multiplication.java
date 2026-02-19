e83a4bb3b16f6a4e2c5ca21ae1fa5f3c76b5f17e
artikamishra <artikamishra20@gmail.com>
2026-02-19 17:31:38 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+public class Multiplication {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        int result;
+
+        result = a * b;
+
+        System.out.println("First number = " + a);
+        System.out.println("Second number = " + b);
+        System.out.println("Multiplication = " + result);
+    }
+}
+
