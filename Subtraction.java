5ba697a089ec17ec88c4c29627cd713fdf0888bf
artikamishra <artikamishra20@gmail.com>
2026-02-19 17:31:13 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,15 @@
+public class Subtraction {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        int result;
+
+        result = a - b;
+
+        System.out.println("First number = " + a);
+        System.out.println("Second number = " + b);
+        System.out.println("Subtraction = " + result);
+    }
+}
+
